20cc6b02d37aeb7dcfba061c40ba335cff3488b6
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2024-05-24 20:13:55 -0400

    nvim gitsigns

---
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,14 @@
+return {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+        local gitsigns = require("gitsigns")
+        gitsigns.setup({
+            numhl = true,
+            current_line_blame = true,
+            current_line_blame_opts = {
+                virt_text_pos = "eol",
+                delay = 0,
+            },
+        })
+    end,
+}

e16d7dc00d8b6fe5c74e5e390b47055ced67de74
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2024-05-27 04:58:54 -0400

    nvim: neotree options

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,26 +1,30 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        require("neo-tree").setup({
-            source_selector = {
-                statusline = true,
-            },
-            sources = {
-                "filesystem",
-                "buffers",
-                "git_status",
-                "document_symbols",
-            },
-            filesystem = {
-                use_libuv_file_watcher = true,
-            },
-        })
-        vim.keymap.set({ "n", "v" }, "<C-n>", ":Neotree filesystem left toggle <cr>", { desc = "Neotree" })
-    end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			source_selector = {
+				statusline = true,
+			},
+			sources = {
+				"filesystem",
+				"buffers",
+				"git_status",
+				"document_symbols",
+			},
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+				},
+				use_libuv_file_watcher = true,
+			},
+		})
+		vim.keymap.set({ "n", "v" }, "<C-n>", ":Neotree filesystem left toggle <cr>", { desc = "Neotree" })
+	end,
 }

34d47019445fe7de2ed21b3fa44a0ec88220b2a7
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2024-05-27 04:44:22 -0400

    nvim: workspaces

---
+++ .config/nvim/lua/plugins/workspaces.lua
@@ -0,0 +1,9 @@
+return {
+    "natecraddock/workspaces.nvim",
+    config = function ()
+        require("workspaces").setup()
+        local telescope = require("telescope")
+        telescope.load_extension("workspaces")
+		vim.keymap.set("n", "<leader>fw", ":Telescope workspaces<cr>", { desc = "Telescope workspaces" })
+    end
+}

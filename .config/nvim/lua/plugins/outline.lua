98d74f505c1041275e6ea0d184ebc44c5f79855a
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2024-05-27 17:31:52 -0400

    nvim: outline

---
+++ .config/nvim/lua/plugins/outline.lua
@@ -0,0 +1,8 @@
+return {
+  "hedyhli/outline.nvim",
+  config = function()
+    vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+
+    require("outline").setup({})
+  end,
+}

4bbac5f50d6ac6b535932fbb30315e97d8337197
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2024-05-27 09:37:49 -0400

    nvim: styles

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,15 @@
-lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+Lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(Lazypath) then
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable",
-    lazypath,
+    Lazypath,
   })
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(Lazypath)
 
 require("vim-options")
 require("lazy").setup("plugins")

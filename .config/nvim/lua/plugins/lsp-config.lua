16b476650604cd55d5811b9a53c8f9889b5f6318
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2024-06-04 14:01:36 -0400

    nvim: added sh lsp and formatter

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,35 +1,48 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls" },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        diagnostics = { globals = { "vim" } },
-                    },
-                },
-            })
-            vim.keymap.set({ "n", "v" }, "<leader>lh", vim.lsp.buf.hover, { desc = "LSP hover" })
-            vim.keymap.set({ "n", "v" }, "<leader>ld", vim.lsp.buf.definition, { desc = "LSP definition" })
-            vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, { desc = "LSP code_action" })
-            vim.keymap.set({ "n", "v" }, "<leader>lf", vim.lsp.buf.format, { desc = "LSP format" })
-        end,
-    },
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local servers = {
+        "bashls", -- bash
+        -- "jedi_language_server", -- python
+      }
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      local mason_lspconfig = require("mason-lspconfig")
+
+      mason_lspconfig.setup({
+        ensure_installed = { "lua_ls" },
+        automatic_installation = true,
+      })
+
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+          },
+        },
+      })
+
+      for _, lsp in ipairs(servers) do
+        lspconfig[lsp].setup({
+          capabilities = capabilities,
+        })
+      end
+
+      vim.keymap.set({ "n", "v" }, "<leader>lh", vim.lsp.buf.hover, { desc = "LSP hover" })
+      vim.keymap.set({ "n", "v" }, "<leader>ld", vim.lsp.buf.definition, { desc = "LSP definition" })
+      vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, { desc = "LSP code_action" })
+      vim.keymap.set({ "n", "v" }, "<leader>lf", vim.lsp.buf.format, { desc = "LSP format" })
+    end,
+  },
 }

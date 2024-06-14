cb9ba44c0de757ec85ba8565bff8174caa0bceb9
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2024-06-14 02:56:49 -0400

    nvim: removed todo-comments, trouble is in charge of TODOs

--- .config/nvim/lua/plugins/todo-comments.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {},
-  config = function()
-    require("todo-comments").setup()
-    vim.keymap.set("n", "<leader>xt", ":TodoTrouble<cr>", { desc = "Trouble TODO" })
-  end,
-}

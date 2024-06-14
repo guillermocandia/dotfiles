694b79ed2e5abf5e2f41d4cdab8a75c96d3fe6d4
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2024-06-14 02:01:21 -0400

    nvim: none-ls configured via mason-null-ls

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,17 +1,34 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,  -- lua
-        null_ls.builtins.formatting.shfmt,   -- bash
-        null_ls.builtins.formatting.black,   -- python
-        null_ls.builtins.formatting.isort,   -- python
-        null_ls.builtins.diagnostics.pylint, -- python
-        -- null_ls.builtins.completion.spell,   -- spell
-      },
-    })
-  end,
-  dependencies = { "nvim-lua/plenary.nvim" },
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "nvimtools/none-ls.nvim",
+    },
+    config = function()
+      require("mason").setup()
+      local mason_null_ls = require("mason-null-ls")
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {},
+      })
+      mason_null_ls.setup({
+        ensure_installed = {
+          "luacheck", -- lua linter
+          "stylua", -- lua formatter
+          "pylint", -- python linter
+          "black", -- python formatter
+          "isort", -- python formatter
+          "shfmt", -- bash formatter
+        },
+        automatic_installation = false,
+        handlers = {},
+      })
+    end,
+  },
 }

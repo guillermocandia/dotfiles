fb77205530ec39f1c3bacff23d811b21df7506a6
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2024-06-22 00:56:15 -0400

    nvim: colorschemes are in their own directory

---
+++ nvim/.config/nvim/lua/plugins/colorschemes/kanagawa.lua
@@ -0,0 +1,7 @@
+return {
+  "rebelot/kanagawa.nvim",
+  name = "kanagawa",
+  lazy = false,
+  priority = 1000,
+  enabled = true,
+}
---
+++ nvim/.config/nvim/lua/plugins/colorschemes/onedarkpro.lua
@@ -0,0 +1,7 @@
+return {
+  "olimorris/onedarkpro.nvim",
+  name = "onedarkpro",
+  lazy = false,
+  priority = 1000,
+  enabled = true,
+}

a47998d56297c2153bcc6170637bb8949bd85e7c
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2024-06-21 21:55:45 -0400

    nvim: added dir for basic config files

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,4 +2,4 @@ vim.g.mapleader = ","
 
 vim.keymap.set("n", "<leader>n", ":bn<cr>", { desc = "Buffer next" })
 vim.keymap.set("n", "<leader>p", ":bp<cr>", { desc = "Buffer prev" })
-vim.keymap.set("n", "<leader>x", ":bx<cr>", { desc = "Buffer close" })
+vim.keymap.set("n", "<leader>d", ":bd<cr>", { desc = "Buffer close" })

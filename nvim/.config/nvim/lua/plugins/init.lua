98b0ec84d75e99a1c17cf68cd274f4b1f7a99d4b
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2024-06-22 13:55:14 -0400

    nvim: plugins dirs

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,3 +1,8 @@
 return {
+  { import = "plugins/treesitter" },
   { import = "plugins/colorschemes" },
+  { import = "plugins/ui" },
+  { import = "plugins/lsp" },
+  { import = "plugins/editor" },
+  { import = "plugins/utils" },
 }

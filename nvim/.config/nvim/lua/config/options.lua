52cd36fb4db0cd227f2415098a49c65a5839559b
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2024-06-23 05:37:13 -0400

    nvim: vim diagnostic icons

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -14,5 +14,12 @@ vim.diagnostic.config({
   },
   underline = false,
   update_in_insert = true,
-  signs = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.INFO] = "",
+      [vim.diagnostic.severity.HINT] = "󰌵",
+    },
+  },
 })

e0f2c612a460e0fd8278f91b5464537cebf627ea
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2024-06-28 09:06:46 -0400

    nvim: treesitter conf

--- nvim/.config/nvim/lua/plugins/treesitter/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter/treesitter.lua
@@ -4,7 +4,11 @@ return {
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
+      modules = {},
+      ensure_installed = "all",
+      sync_install = false,
       auto_install = true,
+      ignore_install = {},
       highlight = { enable = true },
       indent = { enable = true },
     })

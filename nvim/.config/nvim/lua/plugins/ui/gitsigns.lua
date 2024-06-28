17adf0d2272cc420bb14867c1482729a65764873
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2024-06-28 05:28:08 -0400

    nvim: Gitsigns conf

--- nvim/.config/nvim/lua/plugins/ui/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/ui/gitsigns.lua
@@ -3,6 +3,7 @@ return {
   config = function()
     local gitsigns = require("gitsigns")
     gitsigns.setup({
+      signcolumn = true,
       numhl = true,
       current_line_blame = true,
       current_line_blame_opts = {
@@ -10,5 +11,7 @@ return {
         delay = 0,
       },
     })
+    vim.keymap.set({ "n", "v" }, "<leader>gb", ":Gitsigns blame<cr>", { desc = "Blame" })
+    vim.keymap.set({ "n", "v" }, "<leader>gd", ":Gitsigns diffthis<cr>", { desc = "Diff" })
   end,
 }
--- nvim/.config/nvim/lua/plugins/utils/extra/which-key/labels.lua
+++ nvim/.config/nvim/lua/plugins/utils/extra/which-key/labels.lua
@@ -5,6 +5,7 @@ local labels = {
   ["<leader>l"] = { name = "LSP" },
   ["<leader>d"] = { name = "Debugger" },
   ["<leader>dp"] = { name = "Python" },
+  ["<leader>g"] = { name = "Git" },
   ["<leader>t"] = { name = "Trouble" },
   ["<leader>e"] = { name = "ETC" },
 }

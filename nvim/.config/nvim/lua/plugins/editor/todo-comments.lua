219155fa5f9c11093f368711dfea6c7ae4e02aa2
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2024-08-20 20:00:41 -0400

    nvim: todo-comments working

--- nvim/.config/nvim/lua/plugins/editor/todo-comments.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "folke/todo-comments.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  opts = {},
-  enabled = false,
-}
--- FIX: list dont show todos
--- nvim/.config/nvim/lua/plugins/ui/trouble.lua
+++ nvim/.config/nvim/lua/plugins/ui/trouble.lua
@@ -1,15 +1,45 @@
 return {
-  "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("trouble").setup({
-      warn_no_results = false,
-      open_no_results = true,
-    })
-    vim.keymap.set({ "n", "v", "i" }, "<C-d>", ":Trouble diagnostics toggle<cr>", { desc = "Trouble diagnostics" })
-    vim.keymap.set("n", "<leader>ts", ":Trouble symbols toggle focus=false<cr>", { desc = "Trouble symbols" })
-    vim.keymap.set("n", "<leader>tl", ":Trouble lsp toggle focus=false win.position=right<cr>", { desc = "Trouble lsp" })
-    vim.keymap.set("n", "<leader>tq", ":Trouble quickfix toggle<cr>", { desc = "Trouble quickfix" })
-    vim.keymap.set("n", "<leader>tL", ":Trouble locllist toggle<cr>", { desc = "Trouble loclist" })
-  end,
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("trouble").setup({
+        warn_no_results = false,
+        open_no_results = true,
+      })
+      vim.keymap.set(
+        { "n", "v", "i" },
+        "<C-d>",
+        ":Trouble diagnostics toggle<cr>",
+        { desc = "Trouble diagnostics", silent = true }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>ts",
+        ":Trouble symbols toggle focus=false<cr>",
+        { desc = "Trouble symbols", silent = true }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>tl",
+        ":Trouble lsp toggle focus=false win.position=right<cr>",
+        { desc = "Trouble lsp", silent = true }
+      )
+      vim.keymap.set("n", "<leader>tq", ":Trouble quickfix toggle<cr>", { desc = "Trouble quickfix", silent = true })
+      vim.keymap.set("n", "<leader>tL", ":Trouble locllist toggle<cr>", { desc = "Trouble loclist", silent = true })
+    end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {},
+    config = function()
+      local todo = require("todo-comments")
+      todo.setup()
+      vim.keymap.set("n", "<leader>tt", ":Trouble todo<cr>", { desc = "Trouble todo", silent = true })
+      vim.keymap.set("n", "<leader>ft", ":TodoTelescope<cr>", { desc = "Telescope todo", silent = true })
+    end,
+  },
 }

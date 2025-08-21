57e34ca1c3d076ecab16c47c0d10b813683a0b3d
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2025-08-21 12:51:53 -0400

    removed unused scripts

--- xmonad/.xmonad/scripts/get-gpu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gpu_info=$(nvidia-smi --query-gpu "utilization.gpu,utilization.memory,temperature.gpu,fan.speed" --format=csv,noheader,nounits)
-
-read -r gpu_utilization memory_utilization gpu_temperature fan_speed <<<${gpu_info//[,]}
-
-echo "GPU: $gpu_utilization%|$gpu_temperatureºC|$memory_utilization%|$fan_speed%"
--- xmonad/.xmonad/scripts/get-upgradeable.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-icon=""
-color="cyan"
-icon_color="yellow"
-
-upgradeable=$(apt-get -q -y --ignore-hold --allow-change-held-packages --allow-unauthenticated -s dist-upgrade | grep ^Inst | wc -l)
-
-echo "<fc=$icon_color><fn=2>$icon</fn></fc><fc=$color>  $upgradeable</fc>"

b18dda45cf0e894093348d4cf8d2d0d519770003
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2025-11-20 18:18:24 -0300

    xmobar: fix

--- xmobar/.config/xmobar/lib/Customs/Volume.hs
+++ xmobar/.config/xmobar/lib/Customs/Volume.hs
@@ -25,8 +25,8 @@ instance Exec Volume where
         where
             shellCommand = "pactl subscribe|grep --line-buffered \"sink #" ++ sink ++ "\""
             sink = case device of
-                "headphones" -> "1"
-                "speakers" -> "2"
+                "headphones" -> "0"
+                "speakers" -> "1"
                 _ -> undefined
 
 getVolume :: String -> IO String

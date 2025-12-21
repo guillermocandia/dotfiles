38f3bad4589d782c60313669a75b30e0777bab50
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2025-12-21 02:48:12 -0300

    nvim: whichkey

--- nvim/.config/nvim/lua/plugins/utils/which-key.lua
+++ nvim/.config/nvim/lua/plugins/utils/which-key.lua
@@ -9,7 +9,20 @@ return {
   config = function()
     local wk = require("which-key")
     wk.setup()
-    local labels = require("plugins.utils.extra.which-key.labels")
-    wk.register(labels)
+    local labels = {
+      { "<leader>b", group = "BufferLine" },
+      { "<leader>bc", group = "BufferLineClose" },
+      { "<leader>d", group = "Debugger" },
+      { "<leader>dp", group = "Python" },
+      { "<leader>e", group = "ETC" },
+      { "<leader>f", group = "Telescope" },
+      { "<leader>g", group = "Git" },
+      { "<leader>h", group = "Haskell tools" },
+      { "<leader>l", group = "LSP" },
+      { "<leader>n", group = "Noice" },
+      { "<leader>t", group = "Trouble" },
+      { "<leader>s", group = "Grug-far" },
+    }
+    wk.add(labels)
   end,
 }

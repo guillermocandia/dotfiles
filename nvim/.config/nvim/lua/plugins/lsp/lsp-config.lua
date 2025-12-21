35bfe2573c8831ce3b83266284a18f3d50518460
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2025-12-21 01:30:43 -0300

    nvim: lsp config

--- nvim/.config/nvim/lua/plugins/lsp/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lsp-config.lua
@@ -1,83 +1,30 @@
 return {
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
     config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
       local mason_lspconfig = require("mason-lspconfig")
 
-      mason_lspconfig.setup({
-        ensure_installed = { "lua_ls" },
-        automatic_installation = true,
-      })
-
-      -- use none-ls for formatting
-      local lsp_formatting = function(bufnr)
-        vim.lsp.buf.format({
-          filter = function(client)
-            return client.name == "null-ls"
-          end,
-          bufnr = bufnr,
-        })
-      end
-
-      local on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            callback = function()
-              lsp_formatting(bufnr)
-            end,
-          })
-        end
-      end
-
-      -- lua
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = {
-          Lua = {
-            diagnostics = { globals = { "vim" } },
-          },
-        },
-      })
-
-      -- python
-      -- lspconfig.pylsp.setup({
-      --   on_attach = on_attach,
-      --   settings = {
-      --     pylsp ={
-      --       plugins = {
-      --         flake8 = {enabled = true},
-      --       }
-      --     }
-      --   }
-      -- })
-
-
-      local servers = {
+      local lsp_servers = {
+        "lua_ls", -- lua
         "bashls", -- bash
-        -- "pylsp", -- python
-        "basedpyright",
+        "basedpyright", -- python
         "marksman", -- markdown
         "yamlls", -- yaml
         "jsonls", -- json
       }
-      for _, lsp in ipairs(servers) do
-        lspconfig[lsp].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
-        })
+
+      mason_lspconfig.setup({
+        ensure_installed = lsp_servers,
+        automatic_installation = true,
+        automatic_enable = false,
+      })
+
+      for _, lsp in ipairs(lsp_servers) do
+        vim.lsp.enable(lsp)
       end
 
       local opts = { noremap = true, silent = true }
@@ -92,13 +39,34 @@ return {
       opts.desc = "LSP format"
       vim.keymap.set({ "n", "v" }, "<leader>lf", vim.lsp.buf.format, opts)
       opts.desc = "LSP rename"
-      vim.keymap.set({ "n", "v" }, "<leader>rn", vim.lsp.buf.rename, opts)
-
-      local builtin = require("telescope.builtin")
-      opts.desc = "LSP references"
-      vim.keymap.set({ "n", "v" }, "<leader>lR", builtin.lsp_references, opts)
-      opts.desc = "LSP implementations"
-      vim.keymap.set({ "n", "v" }, "<leader>li", builtin.lsp_implementations, opts)
+      vim.keymap.set({ "n", "v" }, "<leader>lr", vim.lsp.buf.rename, opts)
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "nvimtools/none-ls.nvim",
+    },
+    config = function()
+      require("mason").setup()
+      require("null-ls").setup({
+        sources = {}, -- tools not supported by mason
+      })
+      require("mason-null-ls").setup({
+        ensure_installed = {
+          "luacheck", -- lua linter
+          "stylua", -- lua formatter
+          "black", -- python formatter
+          "isort", -- python formatter
+          "shfmt", -- bash formatter
+          "markdownlint", -- markdown linter, formatter
+          "yamlfmt", -- yaml formatter
+        },
+        automatic_installation = true,
+        handlers = {},
+      })
     end,
   },
 }
